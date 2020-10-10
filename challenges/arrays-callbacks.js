e527a1d225b91e379429c1f88c74ebbd8fe6cae0
Jonathan Canola <jonathancanola7@gmail.com>
2020-10-09 20:19:29 -0700

    not final

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -27,7 +27,7 @@ console.log(displayNames);
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
-*/
+*/ 
 
 const lowCaseAnimalNames
 console.log(lowCaseAnimalNames);
